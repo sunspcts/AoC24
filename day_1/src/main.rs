ea4c7c4d347c6f770a0a7a9ef2cbcca34bcef2c4
sunspcts <crynglghtng@gmail.com>
2024-12-02 17:59:41 +0000

    Day 2, Part 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,24 +1,29 @@
+#![warn(clippy::pedantic)]
+#![warn(clippy::style)]
+
 use std::collections::HashMap;
 use std::fs;
 use std::iter::zip;
+
 fn main() {
     let input = fs::read_to_string("input.txt").expect("input.txt does not exist.");
-    let (mut list_a, mut list_b): (Vec<i32>, Vec<i32>) = input.split("\n").map(parse_line).unzip();
-    list_a.sort(); list_b.sort();
+    let (mut list_a, mut list_b): (Vec<i32>, Vec<i32>) = input.lines().map(parse_line).unzip();
+    list_a.sort_unstable(); list_b.sort_unstable();
+
     println!("{}", part_1(&list_a, &list_b));
+
     println!("{}", part_2(&list_a, &list_b));
 }
 
 fn part_1(list_a: &[i32], list_b: &[i32]) -> i32 {
-    zip(list_a, list_b).fold(0, |acc, (x, y)| acc + (x - y).abs())
+    zip(list_a, list_b).map(|(x, y)| (x - y).abs()).sum()
 }
 
 fn part_2(list_a: &[i32], list_b: &[i32]) -> i32 {
     let mut map: HashMap<i32, i32> = HashMap::new();
     for i in list_b {
-        map.entry(*i).and_modify(|x| *x += 1).or_insert(1);
+        *map.entry(*i).or_default() += 1;
     }
-
     list_a.iter().map(|x| 
         map.get(x).map_or(0, |n| n * x)).sum()
 }
@@ -28,8 +33,6 @@ fn parse_line(line: &str) -> (i32, i32) {
     let split: Vec<i32> = line.split_whitespace()
     .map(|n| n.parse::<i32>().expect("Not a Number!"))
     .collect();
-    match split.len() {
-        0 => (0, 0),
-        _ => (split[0], split[1])
-    }
+    assert!(split.len() == 2);
+    (split[0], split[1])
 }
---
+++ day_2/src/main.rs
@@ -0,0 +1,45 @@
+#![warn(clippy::pedantic)]
+#![warn(clippy::style)]
+#![deny(clippy::all)]
+
+use std::fs;
+use itertools::Itertools;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("input.txt does not exist.");
+    let reports: Vec<Vec<i64>> = input.lines().map(parse_line).collect();
+    println!("{}", part_1(&reports));
+}
+
+fn part_1(reports: &[Vec<i64>]) -> usize {
+    reports.iter().filter(|x| safe(x)).count()
+}
+
+fn safe(report: &[i64]) -> bool {
+    match is_monotonic(report) {
+        MonotonicBehaviour::Inc | MonotonicBehaviour::Dec => {
+            report.iter().tuple_windows().all(|(x, y)| (x - y).abs() <= 3 &&  (x - y).abs() >= 1)
+        },
+        MonotonicBehaviour::Neither => false,
+    }
+}
+
+fn is_monotonic(arr: &[i64]) -> MonotonicBehaviour {
+    let inc = arr.iter().tuple_windows().all(|(x, y)| x <= y);
+    let dec = arr.iter().tuple_windows().all(|(x, y)| x >= y);
+    match (inc, dec) {
+        (true, false) => MonotonicBehaviour::Inc,
+        (false, true) => MonotonicBehaviour::Dec,
+        _ => MonotonicBehaviour::Neither
+    }
+}
+
+enum MonotonicBehaviour {
+    Inc, Dec, Neither
+}
+
+fn parse_line(line: &str) -> Vec<i64> {
+    line.split_whitespace()
+    .map(|n| n.parse::<i64>().expect("Not a Number!"))
+    .collect()
+}
